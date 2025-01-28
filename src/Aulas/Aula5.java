0bf5873ecd7651ff899a38c3d2619395efffa23e
Victor Diego <studyvictordiego@gmail.com>
2025-01-28 01:35:53 -0300

    Operadores de comparação

--- src/Aulas/Aula5.java
+++ src/Aulas/Aula5.java
@@ -1,9 +1,11 @@
 package Aulas;
-//operadores aritméticos
-// + - * / %
-// % é o operador de módulo, que retorna o resto da divisão
+
 public class Aula5 {
     public static void main(String[] args){
+        //operadores aritméticos
+        // + - * / %
+        // % é o operador de módulo, que retorna o resto da divisão
+
         int a = 10;
         int b = 2;
 
@@ -12,5 +14,15 @@ public class Aula5 {
         System.out.println(a * b); //multiplicação saida 20
         System.out.println(a / b); //divisão saida 5
         System.out.println(a % b); //resto da divisão saida 0
+
+        //operadores de comparação
+        // == != > < >= <=
+        System.out.println(a == b); //igualdade saida false
+        System.out.println(a != b); //diferença saida true
+        System.out.println(a > b); //maior que saida true
+        System.out.println(a < b); //menor que saida false
+        System.out.println(a >= b); //maior ou igual saida true
+        System.out.println(a <= b); //menor ou igual saida false
+
     }
 }

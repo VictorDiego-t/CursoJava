5521c6c1be4714b29f9635119bac6a5709da9320
Victor Diego <studyvictordiego@gmail.com>
2025-01-28 04:24:01 -0300

    Exercicio 7 Média

---
+++ src/Exercicios/Exercicio7.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+public class Exercicio7 {
+    public static void main(String[] args){
+        int nota1 = 8;
+        int nota2 = 3;
+        int media = (nota1 + nota2) / 2;
+        System.out.println(media);
+        if (media >= 6){
+            System.out.println("Você foi Aprovado");
+        }
+        else{
+            System.out.println("Voce Foi Reprovado");
+        }
+    }
+}

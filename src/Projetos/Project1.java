e0ce7013d2f9ea4a27e4c21ded6bda80c2cf860c
Victor Diego <studyvictordiego@gmail.com>
2025-01-28 03:17:33 -0300

    Projeto Calculadora simples

---
+++ src/Projetos/Project1.java
@@ -0,0 +1,16 @@
+package Projetos;
+
+public class Project1 {
+    public static void main(String[] args){
+        System.out.println("Bem Vindo a minha Primeira Calculadora em java");
+        //Modifique os valores para alterar a soma, subtração, multiplicação e divisão
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println("Soma: " + (num1 + num2));
+        System.out.println("Subtração: " + (num1 - num2));
+        System.out.println("Multiplicação: " + (num1 * num2));
+        System.out.println("Divisão: " + (num1 / num2));
+
+
+    }
+}

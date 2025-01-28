8e69e77f9dd46485a154e58a9842cdfdd17d3a12
Victor Diego <studyvictordiego@gmail.com>
2025-01-28 00:54:40 -0300

    Exercicio1

---
+++ src/Exercicios/Exercicio1.java
@@ -0,0 +1,10 @@
+package Exercicios;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        int quantidadeFarinha = 10; // Quantidade de Farinha
+        int quantidadeAcucar = 5; // Quantidade de Açúcar
+        System.out.println("Quantidade de Farinha: " + quantidadeFarinha); // Exibe a quantidade de Farinha
+        System.out.println("Quantidade de Açúcar: " + quantidadeAcucar); // Exibe a quantidade de Açúcar
+    }
+}

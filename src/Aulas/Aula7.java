b7958135f804770db0116458d5a9daf6de5f0036
Victor Diego <studyvictordiego@gmail.com>
2025-01-28 04:41:09 -0300

    Aula 7 Estruturas De Repetição

---
+++ src/Aulas/Aula7.java
@@ -0,0 +1,23 @@
+package Aulas;
+//Estruturas de repetição
+public class Aula7 {
+    public static void main(String[] args){
+        //Estrutura de repetição for
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+        //Estrutura de repetição while
+        int j = 0;
+        while (j < 10){
+            System.out.println(j);
+            j++;
+        }
+        //Estrutura de repetição do while
+        int k = 0;
+        do{
+            System.out.println(k);
+            k++;
+        }while(k < 10);
+    }
+
+}

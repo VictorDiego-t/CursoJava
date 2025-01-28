cac87acf1fb6e05c6e3696c721d2f002a2c58f6b
Victor Diego <studyvictordiego@gmail.com>
2025-01-28 04:15:59 -0300

    Exercicio6 Dias Do Mes

---
+++ src/Exercicios/Exercicio6.java
@@ -0,0 +1,16 @@
+package Exercicios;
+
+public class Exercicio6 {
+    public static void main(String[] args){
+        String mes = "fevereiro";
+        
+        String diasm = switch (mes) {
+            case "janeiro","março","maio","julho","agosto","outubro","dezembro" -> "31 dias";
+            case "abril","junho","stembro","novembro" -> "30 dias";
+            case "fevereiro" -> "o número de dias é 28 (considerando um ano não bissexto).";
+
+            default -> "Mês desconhecido";
+        };
+        System.out.println(diasm);
+    }
+}

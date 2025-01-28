c50543d4f122cbc3361fc355a6eb57fb99219f1f
Victor Diego <studyvictordiego@gmail.com>
2025-01-28 01:34:21 -0300

    Operadores Aritimeticos

--- src/Aulas/Aula5.java
+++ src/Aulas/Aula5.java
@@ -1,4 +1,16 @@
 package Aulas;
-
+//operadores aritméticos
+// + - * / %
+// % é o operador de módulo, que retorna o resto da divisão
 public class Aula5 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 2;
+
+        System.out.println(a + b); //soma saida 12
+        System.out.println(a - b); //subtração saida 8
+        System.out.println(a * b); //multiplicação saida 20
+        System.out.println(a / b); //divisão saida 5
+        System.out.println(a % b); //resto da divisão saida 0
+    }
 }

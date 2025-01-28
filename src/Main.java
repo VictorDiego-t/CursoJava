29cc3b6420ab092b127566b36843e633446c9a20
Victor Diego <studyvictordiego@gmail.com>
2025-01-27 23:10:46 -0300

    aula2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        byte farinha = -2;
+        System.out.println("A receita requer: " + farinha + " xicaras de farinha");
+
     }
 }

53e468d14184569f3ddff965235bee65d328f43c
Victor Diego <studyvictordiego@gmail.com>
2025-01-28 00:22:21 -0300

    Uso de var em java

---
+++ src/Aula3.java
@@ -0,0 +1,12 @@
+public class Aula3 {
+    public static void main(String[] args) {
+
+        // Uso do var só foi implementado a partir do Java 10
+        //Obrigatório declarar um valor não pode ser nulo
+
+        var numero = 10; // inference int
+        // var usuario = new Usuario(); // inference Usuário
+        System.out.println("Valor do numero é: " + numero); // Funciona sem declarar o tipo resulta 10.
+
+    }
+}

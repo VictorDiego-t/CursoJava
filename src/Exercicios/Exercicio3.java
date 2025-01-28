5f732094349c80ee21285e106011f53c47b945f9
Victor Diego <studyvictordiego@gmail.com>
2025-01-28 03:47:14 -0300

    Exercicio3 Par/impar

---
+++ src/Exercicios/Exercicio3.java
@@ -0,0 +1,13 @@
+package Exercicios;
+
+public class Exercicio3 {
+    public static void main(String[] args){
+        int numero = 3;
+        int par = numero % 2;
+        if (par == 0){
+            System.out.println("O número " + numero + " é par.");
+        } else {
+            System.out.println("O número " + numero + " é ímpar.");
+        }
+    }
+}

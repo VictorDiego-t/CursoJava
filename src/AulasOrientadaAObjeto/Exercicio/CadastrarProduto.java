3dc1596cca86b075614904d9280566421cdbaaf1
Victor Diego <studyvictordiego@gmail.com>
2025-02-02 04:58:00 -0300

    Criação De Produtos Usando Orientação a objetos

--- src/AulasOrientadaAObjeto/Exercicio/CadastrarProduto.java
+++ src/AulasOrientadaAObjeto/Exercicio/CadastrarProduto.java
@@ -1,4 +1,10 @@
 package AulasOrientadaAObjeto.Exercicio;
-
+import AulasOrientadaAObjeto.Exercicio.Produto;
 public class CadastrarProduto {
+
+    public static void main(String[] args) {
+        Produto produto = new Produto("Relogio", 100, 10){
+        };
+        System.out.println(produto.obterProduto());
+    }
 }
--- src/AulasOrientadaAObjeto/Exercicio/Produto.java
+++ src/AulasOrientadaAObjeto/Exercicio/Produto.java
@@ -1,4 +1,19 @@
 package AulasOrientadaAObjeto.Exercicio;
 
 public class Produto {
+
+    private String nome;
+    private int preco;
+    private int quantidadeEmEstoque;
+
+    public Produto(String nomeInit, int precoInit, int QuantidadeEmEstoqueInit){
+        nome = nomeInit;
+        preco = precoInit;
+        quantidadeEmEstoque = QuantidadeEmEstoqueInit;
+    }
+
+    public String obterProduto(){
+        return "Produto: "+ nome + " Preço: " + preco + " R$" + " Estoque: " + quantidadeEmEstoque;
+    }
+
 }
